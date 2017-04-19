cd925d433f181730e4d515f4e2355c5be627c034
celio <celiogomesgouvea@gmail.com>
2017-04-19 08:44:21 -0300

    tela pincipal com acesso a galeria de fotos e acesso a camera. compponentes visuais ja na versão final.

--- app/src/main/java/com/cgtechnology/agortins2017/TelaPrincipal.java
+++ app/src/main/java/com/cgtechnology/agortins2017/TelaPrincipal.java
@@ -1,13 +1,223 @@
 package com.cgtechnology.agortins2017;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    //componente de imagemView
+    private ImageView imagem = null;
+    //componete de button
+    ImageButton selecionar = null;
+    ImageButton foto = null;
+    ImageButton simNao = null;
+
+    //int usado
+    private final  int GALERIA_IMAGEM = 1;
+    private final int PERMISSAO_REQUEST = 2;
+    private final int TIRA_FOTO = 3;
+    private final int CAMERA = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        imagem = (ImageView) findViewById(R.id.ivImagem);
+        selecionar = (ImageButton) findViewById(R.id.btSelecionar);
+        foto = (ImageButton) findViewById(R.id.btCapturar);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
+
     }
+
+
+    public void selecionarImagem(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    public void editarImagem(View view){
+
+
+        AlertDialog alertDialog;
+        alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setMessage("test");
+        // alertDialog.setButton("Sim",simNao);
+        alertDialog.show();
+
+
+    }
+    public void tirarFoto(View view)
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                arquivoFoto = criarArquivo();
+            } catch (IOException ex) {
+                // Manipulação em caso de falha de criação do arquivo
+            }
+            if (arquivoFoto != null) {
+                Uri photoURI = FileProvider.getUriForFile(getBaseContext(),
+                        getBaseContext().getApplicationContext().getPackageName() +
+                                ".provider", arquivoFoto);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA);
+            }
+        }
+    }
+
+
+
+    private File arquivoFoto = null;
+    private File criarArquivo() throws IOException {
+        String timeStamp = new
+                SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File pasta = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File imagem = new File(pasta.getPath() + File.separator
+                + "JPG_" + timeStamp + ".jpg");
+        return imagem;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGEM) {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            imagem.setImageBitmap(thumbnail);
+        }
+        if (requestCode == TIRA_FOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagem.setImageBitmap(imageBitmap);
+        }
+        if (requestCode == CAMERA && resultCode == RESULT_OK) {
+            sendBroadcast(new Intent(
+                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    Uri.fromFile(arquivoFoto))
+            );
+            exibirImagem();
+        }
+    }
+    public void onStart(){
+        super.onStart();
+    }
+    public void onStop(){
+        super.onStop();
+    }
+    public void onResume(){
+        super.onResume();
+    }
+    public void onPause(){
+        super.onPause();
+    }
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        try {
+            if (requestCode == PERMISSAO_REQUEST) {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // A permissão foi concedida. Pode continuar
+                } else {// A permissão foi negada. Precisa ver o que deve ser desabilitado
+                }
+                return;
+            }
+        }catch (Exception e)
+        {
+            Log.i("INFO", "Ecessão = "+e);
+        }
+    }
+
+
+
+    private void exibirImagem() {
+        int targetW = imagem.getWidth();
+        int targetH = imagem.getHeight();
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(arquivoFoto.getAbsolutePath(), bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        Bitmap bitmap =
+                BitmapFactory.decodeFile(arquivoFoto.getAbsolutePath(), bmOptions);
+        imagem.setImageBitmap(bitmap);
+    }
+
+
+    public void alert()
+    {
+        AlertDialog.Builder al = new AlertDialog.Builder(TelaPrincipal.this);
+        al.setMessage("Test mensagem com ações");
+        al.setNegativeButton("NAO", (DialogInterface.OnClickListener) simNao);
+        al.setPositiveButton("SIM", (DialogInterface.OnClickListener) simNao);
+        al.show();
+
+    }
+
+
 }
